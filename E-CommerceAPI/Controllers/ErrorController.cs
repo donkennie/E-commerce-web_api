acdc46f4bd725e9116d3a0e75dd8d35b5760ee33
donkennie <ajeigbekehinde160@gmail.com>
2022-07-28 01:38:52 +0100

    error and buggy controlller

---
+++ E-CommerceAPI/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using E_CommerceAPI.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceAPI.Controllers
+{
+  
+        [Route("errors/{code}")]
+       // [ApiExplorerSettings(IgnoreApi = true)]
+
+        public class ErrorController : BaseApiController
+        {
+            public IActionResult Error(int code)
+            {
+                return new ObjectResult(new ApiResponse(code));
+            }
+        }
+  
+}
+    
